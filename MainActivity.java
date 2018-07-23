f8161d1ff643313cad441d01fd0e3cd3c82e4b7c
ksi-pc110-012-14 <m.sophy42@yahoo.com>
2018-07-23 01:50:36 -0700

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ksi_pc110_012_14.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
